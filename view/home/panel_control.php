ba046ed664b28101ecb5ad6ae39648a1b0971a5f
AndresMb03 <128008296+AndresMb03@users.noreply.github.com>
2023-06-01 08:43:47 -0500

    Update panel_control.php

--- view/home/panel_control.php
+++ view/home/panel_control.php
@@ -1,1 +1,24 @@
+<?php
+    require_once("c://xampp/htdocs/login/view/head/header.php");
+    if(empty($_SESSION['usuario'])){
+        header("Location:login.php");
+    }
+?>
+<div class="fondo-login">
+    <div class="icon">
+        <a href="/login/index.php">
+        <img src="https://virtual2.umng.edu.co/pluginfile.php/1/theme_moove/logo/1677851070/LogosUMNGabiertos.png">
+        </a>
+    </div>
 
+    <div class="login col-3 mt-3">
+        <a href="centro.php" class="letra">Centro de Investigación</a>
+    </div>
+
+    <div class="login col-3 mt-3">
+        <a href="docente.php" class="letra">Docente</a>
+    </div>
+</div>
+<?php
+    require_once("c://xampp/htdocs/login/view/head/footer.php");
+?>

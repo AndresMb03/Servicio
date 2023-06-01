e858b5e90098b44a120b1d5cd4f278b42a93fed6
AndresMb03 <128008296+AndresMb03@users.noreply.github.com>
2023-06-01 08:26:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("c://xampp/htdocs/login/view/head/header.php");
+?>
+
+<div class="fondo-login">
+    <div class="icon">
+        <a href="/login/index.php">
+        <img src="https://virtual2.umng.edu.co/pluginfile.php/1/theme_moove/logo/1677851070/LogosUMNGabiertos.png">
+        </a>
+    </div>
+    <div class="icon">
+        <a href="/login/index.php">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/00/UMNG_Calle_100_Foto_panor%C3%A1mica.jpg">
+        </a>
+    </div>
+</div>
+
+<?php
+    require_once("c://xampp/htdocs/login/view/head/footer.php");
+?>
+

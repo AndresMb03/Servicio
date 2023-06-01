3295c849d4fd8a3c033b1487d6e11f2622c1eaef
AndresMb03 <128008296+AndresMb03@users.noreply.github.com>
2023-06-01 08:44:57 -0500

    Update verificar.php

--- view/home/verificar.php
+++ view/home/verificar.php
@@ -1,1 +1,15 @@
-
+<?php
+    require_once("c://xampp/htdocs/login/controller/homeController.php");
+    session_start();
+    $obj = new homeController();
+    $correo = $obj->limpiarcorreo($_POST['correo']);
+    $contraseña = $obj->limpiarcadena($_POST['contraseña']);
+    $bandera = $obj->verificarusuario($correo,$contraseña);
+    if($bandera){
+        $_SESSION['usuario'] = $correo;
+        header("Location:panel_control.php");
+    }else{
+        $error = "<li>La contraseña es incorrecta</li>";
+        header("Location:login.php?error=".$error);
+    }
+?>

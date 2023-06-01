c709874ce51628b82375971f3400ea1f1dc6d418
AndresMb03 <128008296+AndresMb03@users.noreply.github.com>
2023-06-01 08:38:32 -0500

    Create docente.php

---
+++ view/home/docente.php
@@ -0,0 +1,43 @@
+<?php
+    require_once("c://xampp/htdocs/login/view/head/head.php");
+    if(!empty($_SESSION['usuario'])){
+        header("Location:panel_control.php");
+    }
+?>
+
+<div class="fondo-login">
+    <div class="icon">
+        <a href="/login/index.php">
+        <img src="https://virtual2.umng.edu.co/pluginfile.php/1/theme_moove/logo/1677851070/LogosUMNGabiertos.png">
+        </a>
+    </div>
+    <div class="titulo">
+
+        <div class="container" style="margin-top: 40px;">
+        <a href="panel_control.php">
+                <h1>Estudiantes registrados</h1>
+                
+            </a>
+            <div class="login col-3 mt-3">
+                <div style="margin-top:20px">
+                    <a href="entregados1.php" class="btn_doc">Andres Murcia</a>
+                </div>
+                <div style="margin-top:20px">
+                    <a href="entregados1.php" class="btn_doc"> Juanito Perez</a>
+                </div>
+                <div style="margin-top:20px;text-align: center;">
+                    <a href="entregados1.php" class="btn_doc">   Pablito Nuñez     </a>
+                </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>
+    
+</div>
+
+
+<?php
+    require_once("c://xampp/htdocs/login/view/head/footer.php");
+?>
